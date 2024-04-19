ce4bdf96c459905ef0415671515ca229e1cd1b2b
webmaster0109 <itdepartmentvfo@gmail.com>
2024-04-19 15:11:50 +0530

    app

--- desol_server/settings.py
+++ desol_server/settings.py
@@ -27,12 +27,12 @@ CSRF_TRUSTED_ORIGINS = ['https://www.desolglobal.ae']
 INSTALLED_APPS = [
     # "admin_interface",
     # "colorfield",
-    "unfold",  # before django.contrib.admin
-    "unfold.contrib.filters",  # optional, if special filters are needed
-    "unfold.contrib.forms",  # optional, if special form elements are needed
-    "unfold.contrib.import_export",  # optional, if django-import-export package is used
-    "unfold.contrib.guardian",  # optional, if django-guardian package is used
-    "unfold.contrib.simple_history",  # optional, if django-simple-history package is used
+    # "unfold",
+    # "unfold.contrib.filters",
+    # "unfold.contrib.forms",
+    # "unfold.contrib.import_export",
+    # "unfold.contrib.guardian",
+    # "unfold.contrib.simple_history",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
